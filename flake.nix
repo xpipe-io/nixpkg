c96f2abf55920004c5bac4399d69fa0cace9d0b8
Christopher Schnick <crschnick@xpipe.io>
2025-11-10 17:11:39 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -14,9 +14,10 @@
       nameValuePair = name: value: { inherit name value; };
       genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
       allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      system = "x86_64-linux";
 
       pkgs = import nixpkgs { };
-      xpipe = pkgs.callPackage ./xpipe-ptb/19.0-16/default.nix { };
+      xpipe = pkgs.callPackage ./xpipe-ptb/19.0-16/default.nix { inherit system; };
 
       forAllSystems = f: genAttrs allSystems (system: f rec {
         inherit system;
