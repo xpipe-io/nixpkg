d7b913bdffb43c1939bc82568b59d6e898ffdad8
crschnick <crschnick@xpipe.io>
2025-11-10 18:59:48 +0000

    Update to version 19.0-18

---
+++ xpipe-ptb/19.0-18/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "xpipe-ptb";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , ...
+    } @ inputs:
+    let
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      forAllSystems = f: genAttrs allSystems (system: f rec {
+        inherit system;
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+      });
+    in
+    {
+      packages = forAllSystems ({ system, pkgs, ... }:
+          pkgs.callPackage ./xpipe-ptb/19.0-18/default.nix { }
+      );
+
+      defaultPackage = forAllSystems ({ system, ... }:
+          self.packages.${system}
+      );
+    };
+}
