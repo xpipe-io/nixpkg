2faf67c29335d0a902c4ac5186a51929f4c3d2c6
crschnick <crschnick@xpipe.io>
2025-11-16 10:04:38 +0000

    Update to version 19.0

---
+++ xpipe/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "xpipe";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , ...
+    } @ inputs:
+    let
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      forAllSystems = f: genAttrs allSystems (system: f rec {
+        inherit system;
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+      });
+    in
+    {
+      packages = forAllSystems ({ system, pkgs, ... }:
+          pkgs.callPackage ./19.0/default.nix { }
+      );
+
+      defaultPackage = forAllSystems ({ system, ... }:
+          self.packages.${system}
+      );
+    };
+}
