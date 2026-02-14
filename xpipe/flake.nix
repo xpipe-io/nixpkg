a055079e139c0f0d4db665898a424d1588233c09
crschnick <crschnick@xpipe.io>
2026-02-14 12:08:56 +0000

    Update to version 21.2.1

--- xpipe/flake.nix
+++ xpipe/flake.nix
@@ -23,7 +23,7 @@
     in
     {
       packages = forAllSystems ({ system, pkgs, ... }:
-          pkgs.callPackage ./21.2/default.nix { }
+          pkgs.callPackage ./21.2.1/default.nix { }
       );
 
       defaultPackage = forAllSystems ({ system, ... }:
