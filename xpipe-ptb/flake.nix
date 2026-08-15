58792dfda0284c1b0ee1ee53be297e03de1739d4
crschnick <crschnick@xpipe.io>
2026-08-15 12:14:14 +0000

    Update to version 24.0-37

--- xpipe-ptb/flake.nix
+++ xpipe-ptb/flake.nix
@@ -23,7 +23,7 @@
     in
     {
       packages = forAllSystems ({ system, pkgs, ... }:
-          pkgs.callPackage ./24.0-36/default.nix { }
+          pkgs.callPackage ./24.0-37/default.nix { }
       );
 
       defaultPackage = forAllSystems ({ system, ... }:
